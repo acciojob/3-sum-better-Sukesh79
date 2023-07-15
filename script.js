bed47f439218a4d002c3d576dadbd272f4d4a0a3
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-07-15 23:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort();
+
+	let i, j , sum = Infinity;
+
+	for(let k = arr.length-1; i >= 1; k--){
+		i = 0;
+		j = k-1;
+
+		while(i < j){
+			let currSum = arr[i] + arr[j] + arr[k];
+			if(currSum < target) i++;
+			else if(sum > target) j--;
+			else return target;
+
+			if(Math.abs(currSum - target) < Math.abs(sum - target))
+				sum = currSum;
+
+			// console.log(currSum);
+		}
+	}
+
+	return sum;
+	
 }
 
+
+// let S = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(threeSum(S, 19));
 module.exports = threeSum;
